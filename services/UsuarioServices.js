c2a83ed601b083f40b00b5621de2417814a02f42
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2024-11-20 23:14:04 -0300

    usuario services

--- services/UsuarioServices.js
+++ services/UsuarioServices.js
@@ -5,8 +5,8 @@ import HttpServices from "./HttpServices";
 //criando uma função  basicamente como orientação objeto usando o (extends) que é usado para herdar todos os metodos que foi passado no elemento pai (HttpServices) para poder reutilizar
 export default class ApiUsuarioServices extends HttpServices {
     //criando um metodo async de login
-    async login(usuario, senha){//recebendo o usuario e a senha
-        return this.post('')//executando a chamada 
+    async login(credenciais){//recebendo as credenciais
+        await this.post('/login', credenciais);//vamos fazer a requisição do backend , passando as credencias de login e senha, para que o backend devolva um token de autorização para que possamos fazer as demais requisições
     }
 
     //criando um metodo async de cadastro
