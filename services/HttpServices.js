b2ba2f0b1ed9e73d730ad736c8e53cff662ed32e
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2024-11-22 21:56:48 -0300

    services.js

--- services/HttpServices.js
+++ services/HttpServices.js
@@ -5,7 +5,7 @@ export default class HttpServices{
     constructor() {
         this.axios = axios.create({
             baseURL: process.env.NEXT_PUBLIC_API_URL + '/api'
-        });//usando algumas instancias do axios, como o create passando a baseURL para nao precisar chamar a UrL da env.local sempre que preciso e contatenando o ('/api') que e sempre usado quando fazemos uma chamada
+        });//usando algumas instancias do axios, como o (create) passando a baseURL para nao precisar chamar a UrL da env.local sempre que preciso e contatenando o ('/api') que e sempre usado quando fazemos uma chamada
     }
 
     //vamos criar o metodo POST
