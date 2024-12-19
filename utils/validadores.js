e41eb8c64bc2cb944f6d174d792d28622609287c
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2024-12-18 23:40:40 -0300

    validadores

--- utils/validadores.js
+++ utils/validadores.js
@@ -1,25 +1,35 @@
+//Aqui vamos criar os validadores:
 
-//criando uma validacao para o nome
+
+//criando uma validacão para o nome:
+//criamos uma função(const) chamada validarNome que recebe dentro dela o nome(nome)
 const validarNome = (nome) => {
-    return nome?.toString().length > 2;//se o nome for maior que 2 letras retorna true
+    return nome?.toString().length > 2;//retornamos(return) se existe um nome(nome?) e se esse nome é do tipo string e se tem no minimo 02 caracteres(toString().length > 2).
 }
-//criando uma const para o email
-const validarEmail = (email) =>{
+
+//criando uma validação para o email:
+//criamos uma função(const) chamada validarEmail e passamos o email(email) dentro dela.
+const validarEmail = (email) => {
+    //criamos dentro dela uma outra função(const) chamada emailStr, que ela espera receber se existe algum email(email?) e se esse email é do tipo string(toString())
     const emailStr = email?.toString();
-    return emailStr.length >= 5 && emailStr.includes('@') && emailStr.includes('.');//verifica se tem mais de '5' caracteres, se tem '@' e se tem '.'
+    return emailStr.length >= 5 && emailStr.includes('@') && emailStr.includes('.');//se tem um email e se o email é do tipo string: retornaremos se o email tem no minimo '5' caracteres, 
+    //se entre esses caracteres tem '@' e se tem '.'
 }
 
-//criando uma const para a senha
+//criando uma const para a senha:
+//criamos uma função(const) chamada validarSenha e que recebe dentro dela a senha(senha)
 const validarSenha = (senha) =>{
-    return senha?.toString().length > 3;//validando se tem pelo menos '3' caracteres
+    return senha?.toString().length > 3;//retornmaos se existe uma senha(senha?), se essa senha é do tipo string(toString) e se a senha tem no minimo 3 caracteres(length > 3).
 }
 
-//criando uma const para validar a senha
+//criando uma const para validar a confirmação da senha:
+//criamos uma função(const) chamada validarConfirmacaoDeSenha que recebe dentro de si a (senha e a confirmação)
 const validarConfirmacaoDeSenha = (senha, confirmacao) => {
-    return validarSenha(senha) && senha === confirmacao;
+    return validarSenha(senha) && senha === confirmacao;//retornamos a função validarSenha(senha) que no caso é a função que diz se esta tudo certo na criação da senha,
+    //e dizemos que a senha tem que ser igual e equivalente a confirmação da senha(senha === confirmação).
 }
 
-//agora vamos exportar as validacoes criadas
+//agora vamos exportar as validações criadas:
 export {
     validarNome,
     validarEmail,
