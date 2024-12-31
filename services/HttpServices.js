33d095d047b2f1e93a26283ad0abb39841d4f817
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2024-12-30 22:41:13 -0300

    Update HttpServices.js

--- services/HttpServices.js
+++ services/HttpServices.js
@@ -1,16 +1,28 @@
+//vamos criar um serviço que sera uma classe base para receber os demais serviços da API:
+
+//primeiro importamos tudo oque vamos utilizar:
+
 import axios from "axios"; //importando o axios que sera responsavel pela requisição 
 
-//criando uma classe base para receber os demais serviços da API
+//Define e exporta(export) uma classe(class) chamada (HttpServices) como exportação padrão(default). Isso significa que quando outro arquivo importar este módulo, essa classe será a referência principal.
+//Isso nos permite encapsular funcionalidades relacionadas a requisições HTTP em uma única classe reutilizável.
 export default class HttpServices{
+    //(constructor)Um método especial da classe que é executado automaticamente quando uma instância da classe é criada ou seja 
+    //ele configura o objeto axios, que é uma biblioteca popular para realizar requisições HTTP.
     constructor() {
+        //(this.axios = axios.create) ele cria uma instância personalizada de axios com configurações predefinidas.
         this.axios = axios.create({
-            baseURL: process.env.NEXT_PUBLIC_API_URL + '/api'
-        });//usando algumas instancias do axios, como o (create) passando a baseURL para nao precisar chamar a UrL da env.local sempre que preciso e contatenando o ('/api') que e sempre usado quando fazemos uma chamada
+            //A baseURL é obtida a partir de uma variável de ambiente chamada (NEXT_PUBLIC_API_URL) e complementada com o caminho (/api).
+            baseURL: process.env.NEXT_PUBLIC_API_URL + '/api' 
+        });
+        //em resumo isso Facilita o uso de URLs dinâmicas (com base no ambiente: desenvolvimento, produção, etc.), mantendo o código mais limpo.
     }
 
-    //vamos criar o metodo POST
-    //passando a url ou seja o pef de onde sera feito a requisição e um data que sera o upload
+    //vamos criar o metodo POST:
+    // vamos passar método da classe que envia uma requisição HTTP do tipo POST que tem como parametros  a (url):que é endpoint específico onde a requisição será feita (complementando a baseURL)
+    // e a (data): que são os dados que serão enviados no corpo da requisição o (payload)
     post(url, data){
-        return this.axios,post(url, data);//criamos isso para o caso de trocar essa lib do axios, entao independente da lib que for usado sera efetiva esse metodo
+        return this.axios.post(url, data);
+        //criamos isso para o caso de trocar essa lib do axios, entao independente da lib que for usado será efetiva esse metodo.
     }
 }
