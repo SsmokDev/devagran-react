7a7593ef7ba33dc2f318081b0f5ea5aa42e91c60
Matheus Gabriel dos Santos <168146040+SsmokDev@users.noreply.github.com>
2025-01-18 22:09:45 -0300

    Update HttpServices.js

--- services/HttpServices.js
+++ services/HttpServices.js
@@ -16,6 +16,24 @@ export default class HttpServices{
             baseURL: process.env.NEXT_PUBLIC_API_URL + '/api' 
         });
         //em resumo isso Facilita o uso de URLs dinâmicas (com base no ambiente: desenvolvimento, produção, etc.), mantendo o código mais limpo.
+
+        
+        // Aqui vamos criar um interceptor de requisição. Interceptores permitem que você modifique ou adicione comportamentos antes que a requisição HTTP seja realmente enviada.
+
+        //então vamos adicionar ao axios(this.axios) um interceptador(.interceptors) para cada requisição(.request) e usando o método(.use()) ele recebe uma função que será executada antes de cada requisição.
+        //que nesse caso estamos passando o (config) o config contém as informações da requisição, como o método, URL, cabeçalhos, etc.
+        this.axios.interceptors.request.use((config) => {
+            //vamos criar um fução(const) chamada (token), essa função tenta receber um token de autenticação(getItem('token)) armazenado no localStorage e o token é recuperado com a chave 'token'.
+            //lembrando que o localStorage é uma maneira de armazenar dados no navegador do cliente.
+            const token = localStorage.getItem('token');
+
+            //Aqui vamos verificar see(if) temos o (token) de autenticação:
+            if(token){
+                //se tivermos o token ele será adicionado ao cabeçalho(Authorization) da requisição HTTP.
+                //O formato 'Bearer ' + token segue o padrão Bearer Token de autenticação em APIs RESTful, onde o token de autenticação é precedido pela palavra-chave Bearer (separado por um espaço).
+                config.headers.Authorization = 'Bearer' + token
+            }
+        });
     }
 
     //vamos criar o metodo POST:
@@ -24,5 +42,14 @@ export default class HttpServices{
     post(url, data){
         return this.axios.post(url, data);
         //criamos isso para o caso de trocar essa lib do axios, entao independente da lib que for usado será efetiva esse metodo.
+    }
+
+    //vamos criar um método GET:
+
+    //Este método get(url) simplesmente chama o método get da biblioteca axios e retorna a resposta dessa requisição. 
+    //Quando o código executa this.axios.get(url), ele faz uma requisição GET para o servidor na URL fornecida. 
+    //O que o método faz é envolver a chamada da biblioteca axios em seu próprio método e retornar o resultado.
+    get(url) {
+        return this.axios.get(url);
     }
 }
